3aec426e30a6882b97e3b7c4526a7f0d80624510
Yiyang Zhu <ererandkeke@gmail.com>
2018-02-11 23:55:17 -0500

    implementation of graph using adjacencylist

--- AdjacencyListDemo.java
+++ AdjacencyListDemo.java
@@ -1,14 +1,10 @@
 class AdjacencyListDemo{
     public static void main(String[] args){
         String[] s1 = {"A","B","C","D","E","F","G"};
-        String[] s2 = {"AB","BD","AC","BE","CF","CG",};
-        DirectedGraph dG = new DirectedGraph(s1,s2);
-        int n = dG.vertices.size();
-        AdjacencyList a = new AdjacencyList(s1,s2);
-        a.display();
-        System.out.println("After transpose: ");
-        for(LinkedList each: a.transpose(a.l)){
-            each.display();
-        }
+        String[] s2 = {"AB","BD","AC","BE","CF","CG"};
+        Graph g = new Graph(s1, s2);
+        g.display();
+        g.transpose();
+        g.display();
     }
 }
